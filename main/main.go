720e4c3cf5a5107c045c948a4c9095885300ebdc
mohammedouchkhi <mohammedouchkhi@gmail.com>
2024-09-26 02:24:46 +0100

    fix

--- main/main.go
+++ main/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) != 1 {
-		fmt.Println("ERROR: invalid number of arguments: please enter 'go run ./cmd/ filename.txt'")
+		fmt.Println("ERROR: invalid number of arguments: please enter 'go run ./main/ filename.txt'")
 		return
 	}
 	Colonie, err := src.HandleFormat(args[0])
@@ -18,5 +18,5 @@ func main() {
 		fmt.Printf("ERROR: %s\n", err)
 		return
 	}
-	
+
 }
